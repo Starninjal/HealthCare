fa7423b9ea3d7e02290f7e497e2945bc86d4f186
johnn-vozzi-wmw <johnn.bryan@wmw.com.br>
2024-06-21 20:28:32 -0300

    initial-commit

---
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/FinalProjectJaymeApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_project_jayme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectJaymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectJaymeApplication.class, args);
+	}
+
+}
---
+++ final-project-jayme/src/test/java/com/example/final_project_jayme/FinalProjectJaymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_project_jayme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectJaymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
