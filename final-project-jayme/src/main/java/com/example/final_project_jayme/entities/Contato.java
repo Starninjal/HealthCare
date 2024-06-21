dbbecd7682c38f2bf13108d1d3c287bfd2289db6
LUCAS ORTIZ PRESTES <lucas.prestes@alunos.sc.senac.br>
2024-06-21 20:55:35 -0300

    add entities

---
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Contato.java
@@ -0,0 +1,5 @@
+package com.example.final_project_jayme.entities;
+
+public class Contato {
+    
+}
---
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Paciente.java
@@ -0,0 +1,11 @@
+package com.example.final_project_jayme.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+
+public class Paciente {
+    
+    
+
+}
