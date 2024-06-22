f5182e3d3e958ba07b94945cff9380e2c5f4a35d
LUCAS ORTIZ PRESTES <lucas.prestes@alunos.sc.senac.br>
2024-06-21 21:29:31 -0300

    alter entities

--- final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Consulta.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Consulta.java
@@ -1,5 +1,25 @@
 package com.example.final_project_jayme.entities;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Consulta {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Paciente paciente;
+
 }
--- final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Paciente.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Paciente.java
@@ -1,11 +1,29 @@
 package com.example.final_project_jayme.entities;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Paciente {
-    
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String sexo;
+    private Date dtNascimento;
 
 }
