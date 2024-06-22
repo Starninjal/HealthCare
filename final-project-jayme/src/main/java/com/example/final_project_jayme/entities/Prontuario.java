f2731d0ca82233e68af196b6bd3cf63fe921e4cd
EndryoCordova <endryocordova3@gmail.com>
2024-06-21 21:43:49 -0300

    update entities

--- final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Prontuario.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Prontuario.java
@@ -1,5 +1,25 @@
 package com.example.final_project_jayme.entities;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Prontuario {
-    
+
+    private Long id;
+    private Date dtEntrada;
+    private Date dtAlta;
+    private Date dtNascimento;
+    private String convenio;
+    private Paciente paciente;
+
 }
