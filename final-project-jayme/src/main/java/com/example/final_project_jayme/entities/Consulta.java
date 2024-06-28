8e54dfb0af73e30c7e8a3b1fa3c4540c9219b379
LUCAS ORTIZ PRESTES <lucas.prestes@alunos.sc.senac.br>
2024-06-28 20:03:37 -0300

    updating entities

--- final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Consulta.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Consulta.java
@@ -1,10 +1,15 @@
 package com.example.final_project_jayme.entities;
 
+import java.util.List;
+
 import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +25,14 @@ public class Consulta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Paciente paciente;
+    // @ManyToOne
+    // @JoinColumn(name = "paciente_id")
+    // private Paciente paciente;
 
+    // @OneToMany(mappedBy = "paciente")
+    // private List<Consulta> consultas;
+
+    @ManyToOne // One Consulta belongs to one Paciente
+    @JoinColumn(name = "paciente_id") // Foreign key column in Consulta table
+    private Paciente paciente;
 }
--- final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Paciente.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Paciente.java
@@ -1,11 +1,13 @@
 package com.example.final_project_jayme.entities;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +27,8 @@ public class Paciente {
     private String cpf;
     private String sexo;
     private Date dtNascimento;
+
+    @OneToMany(mappedBy = "paciente")
+    private List<Consulta> consultas; // Use 'consultas' for plural consistency
 
 }
