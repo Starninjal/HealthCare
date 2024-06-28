cd43958411debce0114ad970e5aacc3bcc1c4f62
LUCAS ORTIZ PRESTES <lucas.prestes@alunos.sc.senac.br>
2024-06-28 20:19:39 -0300

    add medicamentos

--- final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Medicamento.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Medicamento.java
@@ -1,5 +1,27 @@
 package com.example.final_project_jayme.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Medicamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
+    private String marca;
+    private String dosagem;
+
+
 }
