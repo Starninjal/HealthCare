a112add27eaf4d2c880de34026496447473bcd0c
LUCAS ORTIZ PRESTES <lucas.prestes@alunos.sc.senac.br>
2024-06-28 22:04:36 -0300

    add entity procedimento

---
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/entities/Procedimento.java
@@ -0,0 +1,29 @@
+package com.example.final_project_jayme.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Procedimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+    private String descricao;
+    private String valor;
+
+}
